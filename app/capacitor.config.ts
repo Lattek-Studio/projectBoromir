62d56f3ecb965df77df6b8f5c6313bd6cae5aa4b
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-04-11 12:55:32 +0300

    added capacitor apps

---
+++ app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.lattek.dust',
+  appName: 'Lattek Dust',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
