58258bba4a169cdd417afaace4e11c8428c6628c
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-04-27 14:25:37 +0300

    dashboard concept

--- app/src/lib/ui/Tab.svelte
+++ app/src/lib/ui/Tab.svelte
@@ -1,4 +1,28 @@
 <script>
     export let name;
+    export let value;
 </script>
-<p>TAB</p>
+<div class="container">
+    <h1>{name}</h1>
+    <p>{value.value}{value.unit}</p>
+    <div class="bar">
+
+    </div>
+</div>
+<style>
+    .container{
+        background-color: var(--bgLight);
+        padding: 1rem;
+        border-radius: 0.5rem;
+        /* margin-block: 1rem; */
+        width: fit-content;
+        display:flex;
+        gap: 1rem;
+        align-items: center;
+    }
+    h1{
+        border-right: 1px solid var(--gray);
+        width: fit-content;
+        padding-right: 1rem;
+    }
+</style>
--- app/src/routes/app/dashboard/+page.svelte
+++ app/src/routes/app/dashboard/+page.svelte
@@ -1,1 +1,58 @@
+<script>
+    import { Tab } from '$ui'
+	import { dbStore } from '$lib/stores/db'
+    export let data;
+    dbStore.subscribe((curr) => {
+		console.log('CURR', curr);
+        if (Object.keys(curr).length === 0) return;
+		const units = {
+            CO2: "ppm",
+            TVOC: "µg/m3",
+            humidity: "%",
+            temperature: "°C",
+            pressure: "Pa",
+            timestamp: '',
+        }
+        Object.keys(curr).forEach(key => {
+            curr[key] = {
+                value: parseFloat(curr[key]).toFixed(2),
+                unit: units[key]
+            }
+        })
+        curr.humidity.value = parseFloat(curr.humidity.value).toFixed(2);
+        curr.temperature.value = parseFloat(curr.temperature.value).toFixed(2);
+        // curr.pressure.value = parseFloat(curr.pressure.value).toFixed(2);
+        data = curr;
+	});
+    data = {
+        "Pressure": {
+            value: 100757.7,
+            unit: "Pa"
+        },
+        "Particles": {
+            value: 3,
+            unit: "ugm3"
+        },
+        "Humidity": {
+            value: 2.3,
+            unit: "%"
+        },
+        "Temperature": {
+            value: 22.975,
+            unit: "°C"
+        },
+    }
+</script>
 <h1>Dashboard</h1>
+<div class="container">
+    {#each Object.entries(data) as [type, value]}
+        <Tab name={type} value={value} />
+    {/each}
+</div>
+<style>
+    .container{
+        display: flex;
+        gap: 1rem;
+        flex-wrap: wrap;
+    }
+</style>
