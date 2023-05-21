03a602608f304fcdd298d8ecdf37e34b6e150241
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-05-21 20:22:06 +0300

    changed structure

--- dustapp/src/routes/+layout.svelte
+++ dustapp/src/routes/+layout.svelte
@@ -1,5 +1,164 @@
 <script>
 	import './styles.css'
-</script>
 
-<slot />
+    import { page } from '$app/stores'
+    import { goto } from '$app/navigation';
+
+    import { Capacitor } from "@capacitor/core";
+	import { StatusBar, Style } from '@capacitor/status-bar';
+    
+    let path;
+    $: path = $page.url.pathname;
+
+   
+  import { onMount } from 'svelte';
+
+	onMount(async () => {
+		if (Capacitor.isPluginAvailable('StatusBar') && Capacitor.getPlatform() !== 'web'){
+			StatusBar.setBackgroundColor({ color: '#071e22' });
+		}
+	});
+</script>
+<nav>
+    <div class="nav">
+        <div class="{path=='/'?'active':''} title" on:click={()=>goto("/")}>
+            <img src="/favicon.png" />
+            <a href="/">Dust</a>
+        </div>
+        <div class="{path=='/'?'active':''} item" on:click={()=>goto("/")}>
+            <img src="/dashboard.png" />
+            <a href="/">Dashboard</a>
+        </div>
+        <div class="{path=='/statistics'?'active':''} item" on:click={()=>goto("/statistics")}>
+            <img src="/statistics.png" />
+            <a href="/statistics">Statistics</a>
+        </div>
+        <div class="{path=='/performance'?'active':''} item" on:click={()=>goto("/performance")}>
+            <img src="/performance.png" />
+            <a href="/performance">Performance</a>
+        </div>
+        <div class="{path=='/app/settings'?'active':''} item" on:click={()=>goto("/settings")}>
+            <img src="/settings.png" />
+            <a href="/settings">Settings</a>
+        </div>
+    </div>
+    <div class="app">
+        <div class="bar">
+            <img class="settings" src="/settings.png"  on:click={()=>goto("/settings")}/>
+            <div class="{path=='/'?'active':''} title" on:click={()=>goto("/")}>
+                <img src="/favicon.png" />
+                <a href="/">Dust</a>
+            </div>
+            <img class="notifications" src="/notifications.png" />
+        </div>
+        <div class="content">
+            <slot />
+        </div>
+    </div>
+</nav>
+<style>
+    nav{
+        display:grid;
+        grid-template-columns: clamp(200px, 15vw, 600px) auto;
+    }
+    img{
+        height: 1.5rem;
+    }
+    .nav{
+        display:flex;
+        flex-direction:column;
+        border-right: 1px solid var(--gray);
+        padding: 1rem 2rem;
+        min-height: 100vh;
+    }
+    .item, .title{
+        display:flex;
+        align-items:center;
+        gap: 1rem;
+        margin-bottom: 1rem;
+    }
+    a{
+        display: inline;
+        text-decoration: none;
+        color: inherit;
+    }
+    .title > a{
+        font-size: 1.5rem;
+        font-weight: bold;
+    }
+    .title{
+        display: flex;
+        margin-bottom: 4rem;
+    }
+    .nav > div:not(.title){
+        opacity: 0.5;
+    }
+    .nav > div.active{
+        /* border-bottom: 1px solid var(--accent); */
+        opacity: 1;
+    }
+    .content{
+        padding: 1rem;
+    }
+    .bar{
+        display: flex;
+        justify-content: flex-end;
+        padding: 1rem;
+    }
+    .bar > .title{
+        display: none;
+        margin-bottom: 0;
+    }
+    .bar > .settings{
+        display: none;
+    }
+    .nav > div{
+        transition: all 0.2s ease-in-out;
+    }
+    .nav > div:not(.title):hover{
+        cursor: pointer;
+        transform: scale(1.1);
+        opacity: 1;
+    }
+    @media (max-width: 600px){
+        nav{
+            display: grid;
+            grid-template-columns: 1fr;
+            grid-template-rows: auto 7.5vh;
+            height: 100vh;
+        }
+        .app{
+            grid-column-start: 1;
+            grid-column-end: 1;
+        }
+        .nav{
+            min-height: 0;
+            grid-row-start: 2;
+            grid-row-end: 2;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            border-right: 0;
+            border-top: 1px solid var(--gray);
+        }
+        .nav > div{
+            margin-bottom: 0;
+            padding: 0;
+        }
+        .nav > div > a{
+            display: none;
+        }
+        .title{
+            display: none;
+        }
+        .bar{
+            justify-content: space-between;
+        }
+        .bar > .title{
+            display: flex;
+        }
+        .bar > .settings, .bar > .notifications{
+            display: block;
+        }
+    }
+</style>
--- dustapp/src/routes/+page.svelte
+++ dustapp/src/routes/+page.svelte
@@ -1,52 +1,23 @@
 <script>
-	import { Tab } from '$ui'
-	import { authStore } from '$lib/stores/auth'
-	import { dbStore } from '$lib/stores/db'
-	import { Capacitor } from "@capacitor/core";
-	import { StatusBar, Style } from '@capacitor/status-bar';
-  import { onMount } from 'svelte';
-
-	onMount(async () => {
-		if (Capacitor.isPluginAvailable('StatusBar') && Capacitor.getPlatform() !== 'web'){
-			StatusBar.setBackgroundColor({ color: '#071e22' });
-		}
-	});
-	let uid;
-	authStore.subscribe((curr) => {
-		console.log('CURR', curr);
-		uid = curr?.currentUser?.uid;
-	});
-	let data
-	dbStore.subscribe((curr) => {
-		console.log('CURR', curr);
-		data = curr;
-	});
-	
+    import { Tab } from '$ui'
+    import { sensors } from '$lib/supabase';
+    
 </script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>Welcome to projectBoromir</h1>
-
-	<p>This project is on its way to beeing finished...</p>
-	<p>...but it's not there yet.</p>
-
-	<p>Your user id is <span>{uid}</span></p>
-	//loop through all keeys of data
-	{#each Object.keys(data) as key}
-		<p>The <span>{key}</span> emission of the world is <span>{data[key]}</span></p>
-	{/each}
-	<Tab />
-	<a href="/app">Go to app</a>
-</section>
-
+<h1>Dashboard v5</h1>
+<!-- {JSON.stringify($sensors)} -->
+{#if $sensors != 0}
+    <div class="container">
+        {#each Object.entries($sensors) as [type, value]}
+            <Tab name={type} value={value} />
+        {/each}
+    </div>
+{:else}
+        <p>Loading...</p>
+{/if}
 <style>
-	section{
-		margin: 0 auto;
-		width: fit-content;
-	}
+    .container{
+        display: flex;
+        gap: 1rem;
+        flex-wrap: wrap;
+    }
 </style>
