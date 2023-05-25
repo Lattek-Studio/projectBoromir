5a8f8f6a4d31a19feb61063679572680ef383ae3
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-05-25 21:08:34 +0300

    testing dc module

--- arduinoCode/actuatos/actuator1/actuator1.ino
+++ arduinoCode/actuatos/actuator1/actuator1.ino
@@ -1,5 +1,16 @@
+#define ENA D3
+#define IN1 D4
+#define IN2 D5
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA,255);
+  delay(2000);
 
 }
 
