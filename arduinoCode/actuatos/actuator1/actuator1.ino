1e1c904ea17306843b30d2c5a7f19568d794b599
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-05-25 21:17:42 +0300

    added init servo positioning

--- arduinoCode/actuatos/actuator1/actuator1.ino
+++ arduinoCode/actuatos/actuator1/actuator1.ino
@@ -1,16 +1,29 @@
+
+
+#include <Servo.h>
+
+//DC Pins
 #define ENA D3
 #define IN1 D4
 #define IN2 D5
+//Servo pin
+#define PWM_SERVO D6
+
+Servo myservo;
 
 void setup() {
+  //Setup DC
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
 
-  digitalWrite(IN1, HIGH);
+  digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
-  analogWrite(ENA,255);
-  delay(2000);
+  analogWrite(ENA,0);
+
+  //Setup Servo
+  myservo.attach(PWM_SERVO);
+  myservo.write(0);
 
 }
 
