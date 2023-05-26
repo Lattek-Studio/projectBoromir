2d1c9c4b4d2cb6545b34058a491196a1de017c24
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-05-26 13:20:59 +0300

    fixed local.properties

--- dustapp/android/app/build/intermediates/assets/debug/public/_app/immutable/chunks/stores.aeafcd18.js
+++
@@ -1,1 +0,0 @@
-import"./index.276b7e77.js";import{s as e}from"./singletons.ea3d55b7.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
