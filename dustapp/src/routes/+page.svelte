736a737d4d90b5d6d427ab50f50cd630491dc790
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-05-26 17:39:04 +0300

    color changes and fixed topbar

--- dustapp/src/routes/+page.svelte
+++ dustapp/src/routes/+page.svelte
@@ -45,33 +45,54 @@
     onDestroy(() => { 
         data = {};
     })
-    function getWord(aqi){
-        if(typeof aqi != 'number'){
-            return '...'
+    function setRootColor(color){
+        // Select the :root element
+        if(typeof window == 'undefined') return
+        const rootElement = document.documentElement;
+
+        // Change the value of the --accent CSS variable
+        rootElement.style.setProperty('--accent', color);
+    }
+    function getWord(aqi) {
+        if (typeof aqi !== 'number') {
+            setRootColor('var(--defaultAccent)');
+            return '...';
         }
-        if(aqi > 300){
-            return 'hazardous'
+
+        if (aqi > 300) {
+            setRootColor('#FF0000'); // Set color for hazardous
+            return 'hazardous';
         }
-        if(aqi > 200){
-            return 'very unhealthy'
+
+        if (aqi > 200) {
+            setRootColor('#990000'); // Set color for very unhealthy
+            return 'very unhealthy';
         }
-        if(aqi > 150){
-            return 'unhealthy'
+
+        if (aqi > 150) {
+            setRootColor('#FF715B'); // Set color for unhealthy
+            return 'unhealthy';
         }
-        if(aqi > 100){
-            return 'sensitive'
+
+        if (aqi > 100) {
+            setRootColor('#FF715B'); // Set color for sensitive
+            return 'sensitive';
         }
-        if(aqi > 50){
-            return 'moderate'
+
+        if (aqi > 50) {
+            setRootColor('#FF715B'); // Set color for moderate
+            return 'moderate';
         }
-        return 'good'
+
+        setRootColor('#FF715B'); // Set color for good
+        return 'good';
     }
 
 </script>
 <div class="container">
     <div class="circle">
         <svg width="274" height="234" viewBox="0 0 274 234" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M270.917 163.673C265.65 190.151 252.65 214.473 233.56 233.563L229.951 229.954C249.619 207.988 261.571 179.022 261.571 147.276C261.571 78.6402 205.702 22.9999 136.785 22.9999C67.8682 22.9999 11.9999 78.6402 11.9999 147.276C11.9999 179.152 24.0498 208.224 43.8604 230.223L40.52 233.563C21.4302 214.473 8.42982 190.151 3.16294 163.673C-2.10394 137.194 0.599213 109.749 10.9306 84.8067C21.2619 59.8645 38.7575 38.5462 61.2048 23.5474C83.6521 8.54855 110.043 0.542969 137.04 0.542969C164.037 0.542969 190.428 8.54855 212.875 23.5474C235.323 38.5462 252.818 59.8645 263.15 84.8066C273.481 109.749 276.184 137.194 270.917 163.673Z" fill="#FF715B"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M270.917 163.673C265.65 190.151 252.65 214.473 233.56 233.563L229.951 229.954C249.619 207.988 261.571 179.022 261.571 147.276C261.571 78.6402 205.702 22.9999 136.785 22.9999C67.8682 22.9999 11.9999 78.6402 11.9999 147.276C11.9999 179.152 24.0498 208.224 43.8604 230.223L40.52 233.563C21.4302 214.473 8.42982 190.151 3.16294 163.673C-2.10394 137.194 0.599213 109.749 10.9306 84.8067C21.2619 59.8645 38.7575 38.5462 61.2048 23.5474C83.6521 8.54855 110.043 0.542969 137.04 0.542969C164.037 0.542969 190.428 8.54855 212.875 23.5474C235.323 38.5462 252.818 59.8645 263.15 84.8066C273.481 109.749 276.184 137.194 270.917 163.673Z" fill="var(--accent)"/>
         </svg>
         <div class="text">
             <h1 class="aqi">aqi</h1>
